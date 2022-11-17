3141095f2fe2e92ee144bc91e83eb29d20e2deb9
Terence Kong <tko.developer.02@gmail.com>
2022-11-17 23:04:03 +0800

    Moved TextArea into its own component.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,9 @@
-export default function Example({ clicked }: any) {
-    const clickPost = () => {
-        console.log("clicked");
-        clicked.value = true;
-    };
+import TextArea from "./components/text-area";
 
+export default function Example({ clicked }: any) {
     return (
         <div className="max-w-xl m-auto pt-3">
-            <div className="flex items-start space-x-4">
-                <div className="min-w-0 flex-1">
-                    <form action="#" className="relative">
-                        <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm ">
-                            <textarea
-                                rows={3}
-                                name="comment"
-                                id="comment"
-                                className="block w-full resize-none p-3 border-0 py-3 focus:outline-none sm:text-sm"
-                                placeholder="Add your improv..."
-                                defaultValue={""}
-                            />
-
-                            <div className="py-2" aria-hidden="true">
-                                <div className="py-px">
-                                    <div className="h-9" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="absolute inset-x-0 bottom-0 flex justify-end py-2 pl-3 pr-2">
-                            <button
-                                type="button"
-                                id="button"
-                                onClick={clickPost}
-                                className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Post
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <TextArea />
         </div>
     );
 }
--- test/app.test.tsx
+++ test/app.test.tsx
@@ -1,12 +1,12 @@
 import { describe, expect, test } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
 import ReactTestUtils from "react-dom/test-utils";
-import Example from "../src/pages/index";
+import TextArea from "../src/pages/components/text-area";
 
 describe("user can post content", () => {
     test("button click", () => {
         const click = { value: false };
-        render(<Example clicked={click} />);
+        render(<TextArea clicked={click} />);
 
         const button = screen.getByRole("button");
         ReactTestUtils.Simulate.click(button);
