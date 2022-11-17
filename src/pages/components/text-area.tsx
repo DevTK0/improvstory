06008a343cd2a13a3205aab42b5de59c59d7be96
Terence Kong <tko.developer.02@gmail.com>
2022-11-17 23:28:41 +0800

    Updated tests to add user input into textbox.

--- src/pages/components/text-area.tsx
+++ src/pages/components/text-area.tsx
@@ -1,7 +1,10 @@
-export default function TextArea({ clicked }: any) {
+import { useRef } from "react";
+
+export default function TextArea({ props }: any) {
+    const textarea = useRef(null);
     const clickPost = () => {
-        console.log("clicked");
-        clicked.value = true;
+        const userInput = (textarea.current! as HTMLTextAreaElement).value;
+        props.value = userInput;
     };
 
     return (
@@ -10,6 +13,7 @@ export default function TextArea({ clicked }: any) {
                 <form action="#" className="relative">
                     <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm ">
                         <textarea
+                            ref={textarea}
                             rows={3}
                             name="comment"
                             id="comment"
@@ -28,7 +32,6 @@ export default function TextArea({ clicked }: any) {
                     <div className="absolute inset-x-0 bottom-0 flex justify-end py-2 pl-3 pr-2">
                         <button
                             type="button"
-                            id="button"
                             onClick={clickPost}
                             className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
--- test/text-area.test.tsx
+++ test/text-area.test.tsx
@@ -4,13 +4,16 @@ import ReactTestUtils from "react-dom/test-utils";
 import TextArea from "../src/pages/components/text-area";
 
 describe("user can post content", () => {
-    test("button click", () => {
-        const click = { value: false };
-        render(<TextArea clicked={click} />);
+    test("post content", () => {
+        const click = { value: "" };
+        render(<TextArea props={click} />);
+
+        const textarea = screen.getByRole("textbox") as HTMLTextAreaElement;
+        textarea.value = "hello world";
 
         const button = screen.getByRole("button");
         ReactTestUtils.Simulate.click(button);
 
-        expect(click.value).toBe(true);
+        expect(click.value).toBe("hello world");
     });
 });
